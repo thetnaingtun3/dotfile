5c10dc5f86e343fa78677d92c5f657531f6008a5
thetnainghtun <thatnaing336@gmail.com>
2024-07-27 11:47:23 +0630

    formater add

--- lua/plugins/blade-nav.lua
+++ lua/plugins/blade-nav.lua
@@ -1,4 +1,12 @@
+-- return {
+--   "ricardoramirezr/blade-nav.nvim",
+--   ft = { "blade", "php" }, -- optional, improves startup time
+-- }
 return {
   "ricardoramirezr/blade-nav.nvim",
+  dependencies = {
+    "hrsh7th/nvim-cmp", -- if using nvim-cmp
+    { "ms-jpq/coq_nvim", branch = "coq" }, -- if using coq
+  },
   ft = { "blade", "php" }, -- optional, improves startup time
 }
