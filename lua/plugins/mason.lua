0816c916a82eb198d7d462fe5f65c5c398a831da
msi <thatnaing336@gmail.com>
2024-09-21 15:39:06 +0630

    lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -70,9 +70,9 @@ return {
     mason_tool_installer.setup({
       ensure_installed = {
         "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
+        "stylua",   -- lua formatter
+        "isort",    -- python formatter
+        "black",    -- python formatter
         "pylint",
         "eslint_d",
       },
